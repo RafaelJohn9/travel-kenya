0c40b55c321ffdaf85143839fa566347496eb4ed
Kennedy <145441761+Dravis-Xam@users.noreply.github.com>
2025-01-28 17:59:05 +0000

    responsive slider

--- components/experiences/src/SliderExper.jsx
+++ components/experiences/src/SliderExper.jsx
@@ -1,6 +1,7 @@
 import { ArrowBigLeftDash, ArrowBigRightDash } from 'lucide-react';
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
+import "./slider.css"
 
 function SliderExperiences({ e }) {
     const [current, setCurrent] = useState(0);
@@ -21,9 +22,31 @@ function SliderExperiences({ e }) {
         return () => clearTimeout(timer); 
     }, [current, e.length]);
 
+    const themes = []; //for research
+
+    const expback = {
+        backgroundColor: {}, //change on each slide
+        backgroundPosition: "center",
+        objectFit: "cover"
+    }
+
+    const removeLocItem = (index, current) => {
+        return e[current].locations.filter(item => item !== removeValue);
+    }
+
     return (
         <div className="slider">
-            <div>{e[current]}</div>
+            <div className='expeContainer'>
+                <div>
+                    <span className='experience-tag'>{e[current].experience}</span>
+                    {e[current].location.map((item, index) => (
+                        <span key={index} className="loc-item">
+                            <button className='remove-loc-item' onClick={removeLocItem(index, current)}></button>
+                            {item}
+                        </span>
+                    ))}
+                </div>
+            </div>
             <button onClick={showPrev} className="sliderBtn">
                 <ArrowBigLeftDash />
             </button>
